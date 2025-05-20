2170a039123a7bb7a86ad47a70bf65c853fda5ca
Paras Khanchandani <dev@paraskcd.com>
2025-05-20 09:13:02 +0200

    Update WallpaperInfoEndpoint.kt

--- app/src/main/java/com/tored/bridgelauncher/api2/server/endpoints/WallpaperInfoEndpoint.kt
+++ app/src/main/java/com/tored/bridgelauncher/api2/server/endpoints/WallpaperInfoEndpoint.kt
@@ -2,21 +2,19 @@ package com.tored.bridgelauncher.api2.server.endpoints
 
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
-import com.tored.bridgelauncher.api2.server.BridgeAPIEndpointWallpaperInfoSignalResponse
 import com.tored.bridgelauncher.api2.server.IBridgeServerEndpoint
-import com.tored.bridgelauncher.api2.server.jsonResponse
 import com.tored.bridgelauncher.services.wallpaper.WallpaperInfo
-import kotlinx.serialization.json.Json
 
 class WallpaperInfoEndpoint(private val _wallpaperInfo: WallpaperInfo): IBridgeServerEndpoint {
     override suspend fun handle(req: WebResourceRequest): WebResourceResponse {
-        return jsonResponse(
-            Json.encodeToString(
-                BridgeAPIEndpointWallpaperInfoSignalResponse.serializer(),
-                BridgeAPIEndpointWallpaperInfoSignalResponse(
-                    wallpaperBase64 = _wallpaperInfo.wallpaperBase64.value.toString()
-                )
-            )
+        val bytes = _wallpaperInfo.wallpaperBytes.value ?: ByteArray(0)
+        return WebResourceResponse(
+            "image/png",
+            "utf-8",
+            200,
+            "OK",
+            mapOf("Content-Type" to "image/png"),
+            bytes.inputStream()
         )
     }
 }
